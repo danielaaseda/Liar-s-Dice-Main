29e7cbac8b1a846e67b995742019b9c491aebf6a
Daniel <daniel.aase.da@gmail.com>
2026-01-29 12:19:31 +0100

    Fixed server with a route for dice colour.

--- Modules/DiceColour.mjs
+++ Modules/DiceColour.mjs
@@ -1,4 +1,7 @@
 export const diceColourChange = function (req, res, next){
+  if(!req.session?.dice){
+    return res.status(401).json("No Dice!");
+  }
   if (req.body.dice && Array.isArray(req.body.dice)) {
     const color = req.body.diceColor || req.user?.diceColor || '#FF5733';
     
--- localHost.mjs
+++ localHost.mjs
@@ -1,14 +1,18 @@
+import express from "express";
 import { diceColourChange } from "./Modules/DiceColour.mjs"
-const express = req('express')
+
+//const express = req('express')
 const app = express()
 const port = 8080
 
-app.use(diceColourChange);
+//app.use(diceColourChange);
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World!')
+  res.send('Server Kjører!')
 })
-
+app.get("/dicecolour", diceColourChange, (req, res, next) =>{
+  res.json ({message: "Colour Changed.", dice: req.dice});
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
