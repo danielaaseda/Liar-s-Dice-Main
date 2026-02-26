fd58c7773e3edc04b93a0bd06ce65c982245edba
Daniel <daniel.aase.da@gmail.com>
2026-02-26 16:54:41 +0100

    added consolelogs

--- Routes/accountManagement.mjs
+++ Routes/accountManagement.mjs
@@ -16,7 +16,7 @@ router.post("/signup", async (req, res) => {
     const result = await signup(req.body);
     res.status(201).json(result);
   } catch (err) {
-    //console.error("FULL ERROR:", err);
+    console.error("FULL ERROR:", err);
     res.status(400).json({ error: err.message });
   }
 });
